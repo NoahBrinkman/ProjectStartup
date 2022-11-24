b66fe247c5dac4a19373f8012bb1672f66b6f033
TygoDev <tygohaan@gmail.com>
2022-11-24 15:47:59 +0100

    latest updates... Im stuck noah help

--- Pet planner app/Assets/Scripts/Game Management/UserManager.cs
+++ Pet planner app/Assets/Scripts/Game Management/UserManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[DisallowMultipleComponent]
 public class UserManager : MonoBehaviour
 {
     [SerializeField] private int happinessValue;
@@ -9,17 +10,24 @@ public class UserManager : MonoBehaviour
     [SerializeField] private int goldValue;
     public static UserManager Instance { get; private set; }
 
-    private void Awake() 
-    { 
+    private void Awake()
+    {
         // If there is an instance, and it's not me, delete myself.
-    
-     if (Instance != null && Instance != this) 
-        { 
-            Destroy(this); 
-        } 
-        else 
-        { 
-            Instance = this; 
-        } 
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public int getGold => goldValue;
+
+    public void setGold(int amount)
+    {
+        goldValue += amount;
     }
 }
--- Pet planner app/Assets/Scripts/ShopManager.cs
+++ Pet planner app/Assets/Scripts/ShopManager.cs
@@ -7,15 +7,23 @@ public class ShopManager : MonoBehaviour
 {
     [SerializeField] private List<ItemInfo> shopItems = new List<ItemInfo>();
     [SerializeField] private GameObject shopItemPrefab = null;
+    [SerializeField] private List<GameObject> shopItemButtons = new List<GameObject>();
 
     private void Start()
     {
-        foreach(ItemInfo item in shopItems)
+        foreach (ItemInfo item in shopItems)
         {
             GameObject newShopItem = Instantiate(shopItemPrefab);
             newShopItem.GetComponent<Button>().interactable = !item.bought;
+            newShopItem.GetComponent<Button>().onClick.AddListener(BuyItem);
             newShopItem.GetComponentInChildren<Text>().text = item.price.ToString();
             newShopItem.transform.SetParent(transform, false);
+            shopItemButtons.Add(newShopItem);
         }
+    }
+
+    private void BuyItem()
+    {
+
     }
 }
